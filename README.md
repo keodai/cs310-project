d1951bbd2f180224f8db8eb4a4f9ebdb42c2733a
keodai <matthew.penny97@gmail.com>
2017-10-16 11:27:33 +0100

    Conversion of all mp3 files in a directory.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 This is the project repository, containing both the project code and documentation.
 
 ### 15/10/2017
-Started implementation, setup of GitHub repository and PyCharm IDE.
-
+* Started implementation, setup of GitHub repository and PyCharm IDE.
 * ffmpeg required for file conversion using Pydub. Otherwise sox will need to be used in the case of stereo audio.
+
+### 16/10/2017
+* Completed method to convert files from mp3 to wav. Modified to work for all files in a directory
--- app.py
+++ app.py
@@ -1,16 +1,23 @@
+import os
 import subprocess
 
-src_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/src/"
-dest_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/dest/"
-src_ext = "mp3"
-dest_ext = "wav"
 
+def mp3ToWav():
+    src_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/src/"
+    dest_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/dest/"
+    src_ext = ".mp3"
+    dest_ext = ".wav"
 
-def mp3ToWav(file):
-    subprocess.call(['ffmpeg', '-i', src_path + file + "." + src_ext, dest_path + file + "." + dest_ext])
+    for file in os.listdir(src_path):
+        print file
+        name = file[:file.rfind(".")]
+        print name
+        src = src_path + name + src_ext
+        dest = dest_path + name + dest_ext
+        subprocess.call(["ffmpeg", "-i", src, dest])
 
 def main():
-    mp3ToWav("000002")
+    mp3ToWav()
 
 if __name__ == '__main__':
     main()
