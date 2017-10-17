15d1fede2aba65ec153b8f45f7bd474affc0651c
keodai <matthew.penny97@gmail.com>
2017-10-17 10:26:57 +0100

    MFCC feature extraction and plots.

--- README.md
+++ README.md
@@ -9,4 +9,12 @@ This is the project repository, containing both the project code and documentati
 * ffmpeg required for file conversion using Pydub. Otherwise sox will need to be used in the case of stereo audio.
 
 ### 16/10/2017
-* Completed method to convert files from mp3 to wav. Modified to work for all files in a directory
+* Completed method to convert files from mp3 to wav. Modified to work for all files in a directory. Could update to use pyDub.
+* Resolved import issues with Anaconda environment, allowing librosa package to be installed for feature extraction.
+* Performed MFCC feature extraction using the librosa package and created plots using matplotlib.pyplot.
+
+### 17/10/2017
+* Updated MFCC feature extraction and plots to be performed for each file in the directory.
+* Plots are now saved to a file, rather than displayed.
+* Added flag to toggle plotting behaviour on or off, so only conversion and feature extraction performed on dataset.
+
--- app.py
+++ app.py
@@ -1,23 +1,50 @@
-import os
-import subprocess
+import os # Filesystem navigation.
+import subprocess # For call to ffmpeg script.
+import librosa # For feature extraction.
+import librosa.display
+import matplotlib.pyplot as plt # For graphs.
+
+PLOT_RESULTS = True
+
+src_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/src/"
+dest_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/dest/"
+plot_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/plots/"
+src_ext = ".mp3"
+dest_ext = ".wav"
+
+file_list = []
 
 
-def mp3ToWav():
-    src_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/src/"
-    dest_path = "/Users/matthew/Documents/university/CS310-Third-Year-Project/cs310-Project/samples/dest/"
-    src_ext = ".mp3"
-    dest_ext = ".wav"
-
+def mp3_to_wav():
     for file in os.listdir(src_path):
-        print file
         name = file[:file.rfind(".")]
-        print name
         src = src_path + name + src_ext
         dest = dest_path + name + dest_ext
+        file_list.append(dest)
         subprocess.call(["ffmpeg", "-i", src, dest])
 
+
+def feature_extraction():
+    y = [0] * len(file_list)
+    sr = [0] * len(file_list)
+    mfcc = [0] * len(file_list)
+
+    for i, file in enumerate(file_list):
+        y[i], sr[i] = librosa.load(file)
+        mfcc[i] = librosa.feature.mfcc(y=y[i], sr=sr[i])
+        if PLOT_RESULTS:
+            plt.figure(num=i, figsize=(10, 4))
+            librosa.display.specshow(mfcc[i], x_axis="time")
+            plt.colorbar()
+            plt.title("MFCC " + str(i))
+            plt.tight_layout()
+            plt.savefig(plot_path + "temp" + str(i) + ".png")
+
+
 def main():
-    mp3ToWav()
+    mp3_to_wav()
+    feature_extraction()
+
 
 if __name__ == '__main__':
     main()
